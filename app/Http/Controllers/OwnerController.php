6fbea0b32e47d79750c43dbb08c2f9652775a564
Edson Santos <edsonsantos@gmail.com>
2017-07-10 15:43:43 -0300

    Colocado proprietários em ordem alfabética

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -25,7 +25,7 @@ class OwnerController extends Controller
 
     public function index(){
 
-    	$owners = \App\Owner::with('cars', 'phones')->orderBy('id', 'desc')->paginate(env('PAGINATION_ITEMS', 20));
+    	$owners = \App\Owner::with('cars', 'phones')->orderBy('name', 'asc')->paginate(env('PAGINATION_ITEMS', 20));
 
         $lateOwners = $this->PaymentController->latePayment();
 
